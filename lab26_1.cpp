75f8e64e6d95864273eaf7de9da5ae9fc8edd8ae
gamxu <wuttipat.did@gmail.com>
2022-02-18 13:35:06 +0700

    lab26_1 completed

--- lab26_1.cpp
+++ lab26_1.cpp
@@ -63,4 +63,46 @@ void List::append(int d){
 	size++;
 }
 
-//Write List::remove() here
+void List::remove(int idx){
+	Node* temp1 = root;
+	if(idx==0){
+		root = temp1->next;
+		delete(temp1);
+		return;
+	}
+	for(int i = 0; i < idx-1;i++){
+		temp1 = temp1->next;
+	}
+	Node* temp2 = temp1->next;
+	temp1->next = temp2->next;
+	delete (temp2);
+}
+
+int main(){
+	List myList = {0,0};
+	myList.append(5);
+	myList.append(7);
+	myList.append(11);
+	myList.append(4);
+	myList.append(12);
+	myList.append(45);
+
+	myList.insert(29,2);
+	myList.insert(33,3);
+	myList.insert(77,5);
+	myList.insert(69,0);
+	myList.show();
+	cout << "\n"; 
+
+	myList.remove(2);
+	myList.show();
+	cout << "\n"; 
+	myList.remove(4);
+	myList.show();
+	cout << "\n";
+	myList.remove(0);
+	myList.show();
+	cout << "\n";
+	
+	return 0;
+}
